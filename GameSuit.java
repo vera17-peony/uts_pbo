9e8c0d8a9028f209881c39f995d5223bec936142
vera17-peony <veraameliasafitri988@gmail.com>
2025-11-08 12:11:09 +0700

    Add files via upload

---
+++ GameSuit.java
@@ -0,0 +1,51 @@
+package uts_pbo;
+import java.util.Random;
+import java.util.Scanner;
+
+class SuitGame {
+    String[] pilihan = {"batu", "gunting", "kertas"};
+    Random random = new Random();
+
+    // Method untuk komputer memilih
+    String pilihanKomputer() {
+        return pilihan[random.nextInt(pilihan.length)];
+    }
+
+    // Method untuk menentukan pemenang
+    String tentukanPemenang(String pemain, String komputer) {
+        if (pemain.equals(komputer)) {
+            return "Seri!";
+        } else if (
+            (pemain.equals("batu") && komputer.equals("gunting")) ||
+            (pemain.equals("gunting") && komputer.equals("kertas")) ||
+            (pemain.equals("kertas") && komputer.equals("batu"))
+        ) {
+            return "Kamu MENANG!";
+        } else {
+            return "Kamu KALAH!";
+        }
+    }
+}
+
+public class GameSuit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SuitGame game = new SuitGame();
+
+        System.out.println("===  GAME SUIT JAVA  ===");
+        System.out.print("Pilih (batu / gunting / kertas): ");
+        String pilihanPemain = input.nextLine().toLowerCase();
+        
+        while (!pilihanPemain.equals("batu") && !pilihanPemain.equals("gunting") && !pilihanPemain.equals("kertas")) {
+                System.out.println("Pilihan tidak valid! Coba lagi.");
+        System.out.print("Pilih (batu / gunting / kertas): ");
+        pilihanPemain = input.nextLine().toLowerCase();
+                }
+        String pilihanKomputer = game.pilihanKomputer();
+
+        System.out.println("Kamu memilih: " + pilihanPemain);
+        System.out.println("Komputer memilih: " + pilihanKomputer);
+        System.out.println("--------------------------");
+        System.out.println(game.tentukanPemenang(pilihanPemain, pilihanKomputer));
+    }
+}
